fbadf0d95c6c114fc5534f2bdd3b78f4c5638b49
Jose Zamudio <zamudio@outlook.com>
2015-02-12 15:44:53 -0800

    Add amount of deaths per year

--- client/main.js
+++ client/main.js
@@ -1,1 +1,22 @@
-console.log('hello, world');
+/* global _ */
+
+'use strict';
+
+$(document).ready(init);
+
+function init() {
+  $('#get-data').click(clickGetData);
+}
+
+function clickGetData() {
+  var year = $('#year').val();
+  var deathBy = $('#disease').val();
+  var url = 'https://cdph.data.ca.gov/resource/kbup-p858.json?year=' + year;
+
+  $.getJSON(url, function(res) {
+    var deaths = _.map(res, function(o) { return o[deathBy] * 1; });
+    deaths = _.compact(deaths);
+    deaths = _.reduce(deaths, function(prev, curr) { return prev + curr; });
+    console.log(deaths);
+  });
+}
